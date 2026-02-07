87755a557716dede4ca2d053b219480e0bee810a
Staceymoe <staceymoe1@gmail.com>
2026-02-07 15:02:48 -0600

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { ChatOpenAI } = require('langchain/chat_models/openai');
 const { SystemMessage, HumanMessage } = require('langchain/schema');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
